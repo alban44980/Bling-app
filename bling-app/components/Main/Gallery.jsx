4c49ab2eca039159e36007898865044f9e73b45f
Alban Mansord <alban.mansord@gmail.com>
2021-11-06 18:51:06 +0100

    search component with basic styling

--- bling-app/components/Main/Gallery.jsx
+++ bling-app/components/Main/Gallery.jsx
@@ -1,7 +1,7 @@
 import styles from '../../styles/Main/Gallery.module.css';
 
 function Gallery() {
-  return <div>I AM THE GALLERY</div>;
+  return <div className={styles.container}>I AM THE GALLERY</div>;
 }
 
 export default Gallery;
--- bling-app/components/Main/Search.jsx
+++ bling-app/components/Main/Search.jsx
@@ -1,7 +1,28 @@
 import styles from '../../styles/Main/Search.module.css';
 
 function Search() {
-  return <div className={styles.container}>I AM THE SEARCH</div>;
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log('FORM SUBMITTED YIII');
+  }
+  return (
+    <div className={styles.container}>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <label htmlFor="searchInput"></label>
+        <input
+          id="searchInput"
+          type="text"
+          placeholder="Search an image: mountains, ocean, or anything else !"
+        />
+        <button>Search</button>
+      </form>
+      <img
+        className={styles.profilePic}
+        src="profile_pic.jpeg"
+        alt="Profile Picture"
+      />
+    </div>
+  );
 }
 
 export default Search;
