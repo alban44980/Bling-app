8b05c4c5e509becf5b066f6a5383205e363db949
Alban Mansord <alban.mansord@gmail.com>
2021-11-06 18:13:20 +0100

    navigation and profile styling

--- bling-app/components/Profile/Navigation.jsx
+++ bling-app/components/Profile/Navigation.jsx
@@ -1,7 +1,40 @@
 import styles from '../../styles/Profile/Navigation.module.css';
 
 function Navigation() {
-  return <div className={styles.container}></div>;
+  return (
+    <div className={styles.container}>
+      <ul className={styles.nav}>
+        <li className={styles.navItem}>
+          <img src="home.png" alt="Home" />
+          <p>Home</p>
+        </li>
+        <li className={styles.navItem}>
+          <img src="photos.png" alt="My photos" />
+          <p>My Photos</p>
+        </li>
+        <li className={styles.navItem}>
+          <img src="heart.png" alt="My likes" />
+          <p>My likes</p>
+        </li>
+        <li className={styles.navItem}>
+          <img src="send.png" alt="My Messages" />
+          <p>Messages</p>
+        </li>
+        <li className={styles.navItem}>
+          <img src="settings.png" alt="Settings" />
+          <p>Settings</p>
+        </li>
+        <li className={styles.navItem}>
+          <img src="Stats.png" alt="Stats" />
+          <p>Stats</p>
+        </li>
+        <li className={styles.navItemLast}>
+          <img src="privacy.png" alt="Privacy and support" />
+          <p>Privacy and Support</p>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default Navigation;
