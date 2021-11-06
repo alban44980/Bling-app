b5e8dd2bd1bfea3699896343a9ebf88d4cf36ab9
Alban Mansord <alban.mansord@gmail.com>
2021-11-06 19:00:59 +0100

    bottom main basic structure and styling

--- bling-app/components/Main/Bottom.jsx
+++ bling-app/components/Main/Bottom.jsx
@@ -1,7 +1,16 @@
 import styles from '../../styles/Main/Bottom.module.css';
 
 function Bottom() {
-  return <div>I AM THE BOTTOM</div>;
+  function handleClick() {
+    console.log('button clicked yiiii');
+  }
+
+  return (
+    <div className={styles.container}>
+      <h1>Not liking those ?</h1>
+      <button onClick={handleClick}>Click me to load new pics !</button>
+    </div>
+  );
 }
 
 export default Bottom;
