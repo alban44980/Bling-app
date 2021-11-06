870f8f0a2df9e97b44820c763cbe9370a2479ee6
Alban Mansord <alban.mansord@gmail.com>
2021-11-06 17:07:12 +0100

    boilerplate clean up and basic structure

---
+++ bling-app/components/Main.jsx
@@ -0,0 +1,7 @@
+import styles from '../styles/Main.module.css';
+
+function Main() {
+  return <div className={styles.container}>hello from main</div>;
+}
+
+export default Main;
---
+++ bling-app/components/Profile.jsx
@@ -0,0 +1,7 @@
+import styles from '../styles/Profile.module.css';
+
+function Profile() {
+  return <div className={styles.container}>HELLO FROM PROFILE</div>;
+}
+
+export default Profile;
